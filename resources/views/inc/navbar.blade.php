550cc2a87c043c6cad16e8734de4463f7923c27b
Justas Arštikaitis <xjustass@gmail.com>
2020-05-09 11:01:53 +0300

    Pataisytas navbar

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,9 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-custom">
-<<<<<<< HEAD
   <a class="navbar-brand" href="/"> VDU Games</a>
-=======
-  <a class="navbar-brand" href="#">VDU Games</a>
->>>>>>> 6d42778ffb193562b3c423e6bf5bbd9ad49afd7b
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
