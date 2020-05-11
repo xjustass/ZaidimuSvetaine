04ce1854b1a04f64ebe13192f9adaac77d2b5d34
Justas Arštikaitis <xjustass@gmail.com>
2020-05-11 18:46:27 +0300

    Pridėta žaidimų statistika iš Steam API

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,7 +7,30 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function index(){
-        return view('pages.index');
+        //gta v
+        $game1 = simplexml_load_file('https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=271590&format=xml');
+        //cs gp
+        $game2 = simplexml_load_file('https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=730&format=xml');
+        //Rust
+        $game3 = simplexml_load_file('https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=252490&format=xml');
+        //Teraria
+        $game4 = simplexml_load_file('https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=105600&format=xml');
+        //Mount and blade
+        $game5 = simplexml_load_file('https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=261550&format=xml');
+       
+        
+        $players_count = array(
+            'game1_count'=> $game1->player_count,
+            'game2_count'=>$game2->player_count,
+            'game3_count'=>$game3->player_count,
+            'game4_count'=>$game4->player_count,
+            'game5_count'=>$game5->player_count
+            );
+
+        
+       // return view('pages.index', compact('game1_count'));
+
+        return view('pages.index')->with($players_count);
     }
 
     public function about(){
