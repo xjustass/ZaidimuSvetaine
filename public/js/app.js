df4156eed11adefd3b1df58dfa5b00c8a8db55e4
Deividas V <vaitdeiv@gmail.com>
2020-05-13 12:54:20 +0300

    Game tiles dabar responsive, scalina, bet reikia kad jie vienodo dydzio butu

--- public/js/app.js
+++ public/js/app.js
@@ -37127,6 +37127,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 __webpack_require__(/*! C:\Users\Deividas\TestPage\resources\js\app.js */"./resources/js/app.js");
 module.exports = __webpack_require__(/*! C:\Users\Deividas\TestPage\resources\sass\app.scss */"./resources/sass/app.scss");
 
+
 /***/ })
 
 /******/ });
