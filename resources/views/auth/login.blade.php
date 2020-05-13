9a06283461cabb97291dbc1edfd427c6af23abf9
Saulius <svel.saulius@gmail.com>
2020-05-13 13:23:20 +0300

    Rergister puslapy pataisyta lenteles isvaizda

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -75,4 +75,32 @@
         </div>
     </div>
 </div>
+
+
+<!-- Footer'is
+
+<div>
+    <footer class="footer my-auto py-3 bg-custom">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-2 textgray" style="font-size: 50px">
+                    <strong>Logo</strong>
+                </div>
+                <div class="col-8">
+
+                </div>
+                <div class="col-2 textgray">
+                    <div>Categories</div>
+                    <div>About us</div>
+                    <div>Sign in/Log in</div>
+                    <div>My Account</div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
+
+-->
+
+
 @endsection
