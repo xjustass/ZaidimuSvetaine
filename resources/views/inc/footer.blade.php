4670226945ead2d45e1e3405aa558dc64fc69d59
Deividas V <vaitdeiv@gmail.com>
2020-05-16 12:43:13 +0300

    Bandziau pataisyt ta tarpa ant telu kairej pusej

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -3,17 +3,18 @@
     <footer class="footer py-3 bg-custom">
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-2 col-xs-5 textgray" style="font-size: 50px">
-                    <strong>Logo</strong>
+                <div class="col-6 textgray" style="font-size: 50px">
+                    <p><strong>Logo</strong></p>
                 </div>
-                <div class="col-8 col-xs-5">
-
+                <div class="col-2">
                 </div>
-                <div class="col-2 col-xs-5 textgray">
-                    <div>Categories</div>
-                    <div>About us</div>
-                    <div>Sign in/Log in</div>
-                    <div>My Account</div>
+                <div class="col-3 textgray">
+                    <div><p style="line-height: 0px">Categories</p></div>
+                    <div><p style="line-height: 0px">About us</p></div>
+                    <div><p style="line-height: 0px">Sign in/Log in</p></div>
+                    <div><p style="line-height: 0px">My Account</p></div>
+                </div>
+                <div class="col-1">
                 </div>
             </div>
         </div>
